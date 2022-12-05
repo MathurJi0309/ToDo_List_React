81f859056c19fd69ebf57a0f8efec138dd396c03
MathurJi <anileshjjssacademy84@gmail.com>
2022-12-05 10:50:36 +0530

    set the api call through the axios =

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+import axios from 'axios';
 
 function App() {
+  const [items, setItems] = useState([
+    {
+    }
+  ]);
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  const listitms=async()=>{
+    const response=await axios.get("https://jsonplaceholder.typicode.com/todos")
+    setItems(response.data)
+  }
+
+  useEffect(()=>{
+    listitms();
+  },[]);
+
+  console.log(items);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
